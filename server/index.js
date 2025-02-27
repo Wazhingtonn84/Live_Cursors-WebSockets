cb2941b1cec422f709517bad74d7df0dd64bba8a
Emmanuel <wazhingtonn84@gmail.com>
2025-02-27 23:51:55 +0300

    Setting up functions in server and WebSockets

--- server/index.js
+++ server/index.js
@@ -4,15 +4,41 @@ const { WebSocketServer } = require("ws");
 const PORT = process.env.PORT || 5000;
 const uuidv4 = require("uuid").v4;
 
-// We instantiate the HTTP handshake and WS
+// We instantiate the HTTP hadshake and WS
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
-
-//Event Handlers
 
 //Accept user requests and identify them by their credentials
 const connections = {}; //connections dictionary
 const users = {}; //users dictionary
+
+//Event Handlers
+const broadcast = () => {
+	Object.keys(connections).forEach((uuid) => {
+		const connection = connections[uuid];
+		const message = JSON.stringify(users);
+		connection.send(message);
+	});
+};
+const handleMessage = (bytes, uuid) => {
+	//we receive bytes as message
+	const message = JSON.parse(bytes.toString());
+	const user = users[uuid];
+	user.state = message;
+
+	broadcast();
+
+	console.log(
+		`${user.username} updated their state: ${JSON.stringify(user.state)}`
+	);
+};
+const handleClose = (uuid) => {
+	console.log(`${users[uuid].username} disconnected`);
+	delete connections[uuid]; // delete keyword to remove the reference
+	delete users[uuid];
+
+	broadcast();
+};
 
 wsServer.on("connection", (connection, request) => {
 	//ws://localhost:5000?username=Keem  =>> Identifying the user
