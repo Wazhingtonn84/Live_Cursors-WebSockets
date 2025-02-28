ed7507237657a616cf90adc39a00b8f8892a1391
Emmanuel <wazhingtonn84@gmail.com>
2025-03-01 00:07:05 +0300

    Added client and some components

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import useWebSocket from "react-use-websocket";
+import { useEffect, useRef } from "react";
+import throttle from "lodash.throttle";
 
 export function Home({ username }) {
-	const WS_URL = "ws://localhost:5000";
-	const x = useWebSocket(WS_URL, {
+	const WS_URL = "ws://localhost:5000"; //connect to server
+	const { sendJsonMessage } = useWebSocket(WS_URL, {
 		queryParams: { username },
 	});
+
+	const THROTTLE = 50;
+	const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
+
+	useEffect(() => {
+		window.addEventListener("mousemove", (e) => {
+			sendJsonMessageThrottled.current({
+				x: e.clientX,
+				y: e.clientY,
+			});
+		});
+	}, []);
 
 	return <h1>Welcome, {username}!</h1>;
 }
